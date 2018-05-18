3880a24c4e01591998a46840a9643a15ac7a5941
Dmytro Iakovliev <dmytro.iakovliev@dev.zodiac.tv>
2018-05-18 11:26:17 +0300

    Add SGS engine logging

--- depman/sgsengine.cpp
+++ depman/sgsengine.cpp
@@ -26,6 +26,8 @@
  *
  */
 
+#include <logging.h>
+
 #include "sgsengine.h"
 
 namespace sgs { namespace lib {
@@ -34,19 +36,30 @@ Engine::Engine()
     : attached_(false)
     , ctx_(0)
 {
+    LOGD << "Create new SGS engine." << ELOG;
     ctx_ = sgs_CreateEngine();
+    LOGT << "SGS: created engine: " << ctx_ << ELOG;
 }
 
 Engine::Engine(sgs_Context* ctx)
     : attached_(true)
     , ctx_(ctx)
 {
+    LOGD << "Attach to existing SGS engine." << ELOG;
+    LOGT << "SGS: attached engine: " << ctx_ << ELOG;
 }
 
 Engine::~Engine()
 {
-    if (!attached_) {
+    LOGT << "SGS: engine: " << ctx_ << " attached: " << attached_ << ELOG;
+    if (!attached_) 
+    {
         sgs_DestroyEngine(ctx_);
+        LOGD << "Destroyed SGS engine." << ELOG;
+    } 
+    else 
+    {
+        LOGD << "Skip destroing SGS engine for attached context." << ELOG;
     }
 }
 
@@ -54,43 +67,65 @@ void Engine::load_lib(Engine::Libs lib)
 {
     switch (lib) {
     case Fmt:
+    {
         sgs_LoadLib_Fmt(ctx_);
+        LOGD << "SGS: Load Fmt lib." << ELOG;
+    }
     break;
     case IO:
+    {
         sgs_LoadLib_IO(ctx_);
+        LOGD << "SGS: Load IO lib." << ELOG;
+    }
     break;
     case Math:
+    {
         sgs_LoadLib_Math(ctx_);
+        LOGD << "SGS: Load Math lib." << ELOG;
+    }
     break;
     case OS:
+    {
         sgs_LoadLib_OS(ctx_);
+        LOGD << "SGS: Load OS lib." << ELOG;
+    }
     break;
     case RE:
+    {
         sgs_LoadLib_RE(ctx_);
+        LOGD << "SGS: Load RE lib." << ELOG;
+    }
     break;
     case String:
+    {
         sgs_LoadLib_String(ctx_);
+        LOGD << "SGS: Load String lib." << ELOG;
+    }
     break;
     };
 }
 
 void Engine::exec_file(const std::string& filename)
 {
+    LOGD << "SGS: Exec file: " << filename << ELOG;
     sgs_ExecFile(ctx_, filename.c_str());
 }
 
 void Engine::func_name(const std::string& fn)
 {
+    LOGT << "SGS: Set func name: " << fn << ELOG;
     sgs_FuncName(ctx_, fn.c_str());
 }
 
 /*static*/ Engine::Ptr Engine::attach(sgs_Context* ctx)
 {
+    LOGT << "SGS: Attach to engine: " << ctx << ELOG;
     return Ptr(new Engine(ctx));
 }
 
 /*static*/ Engine::Ptr Engine::attach(sgs_Context* ctx, const std::string& fn)
 {
+    LOGT << "SGS: Attach to engine: " << ctx << " func_name: " << fn <<  ELOG;
     Ptr result = Ptr(new Engine(ctx));
     result->func_name(fn);
     return result;
@@ -99,11 +134,13 @@ void Engine::func_name(const std::string& fn)
 // Stack
 void Engine::push_bool(sgs_Bool value)
 {
+    LOGT << "SGS: Push bool: " << value <<  ELOG;
     sgs_PushBool(ctx_, value);
 }
 
 sgs_Context* Engine::c_context()
 {
+    LOGW << "SGS: !!! Access to raw context: " << ctx_ <<  ELOG;
     return ctx_;
 }
 
